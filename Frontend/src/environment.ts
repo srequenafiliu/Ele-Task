3e165c23afe6541c4f139c57c59bc508d51025e5
Sandra <sandrarequena1999@gmail.com>
2024-02-07 18:52:22 +0100

    Cambio de link

--- Frontend/src/environment.ts
+++ Frontend/src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://ele-task.srequenafiliu.repl.co'
+  baseUrl: 'https://00b647c9-a978-4a64-a545-cbeca93d8a43-00-2e7u5s8ms9hz4.spock.replit.dev'
 };
